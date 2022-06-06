3672dc422e777fb24301cf5234b33b7fccda5a3b
tjs6595 <t.schultejans@schenckprocess.com>
2022-06-05 20:50:29 -0500

    Adding remaining stub routes.

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,8 @@
 // Require needed modules.
 const router = require('express').Router()
 
-// More code here in a moment
+router.get('/', (req, res) => {
+    res.send('GET /places')
+})
 
 module.exports = router
