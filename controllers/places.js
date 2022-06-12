b7775ca274d269769b8677a86d59b316d5dce810
tjs6595 <t.schultejans@schenckprocess.com>
2022-06-12 11:32:32 -0500

    Delete functionality implemented and working.

--- controllers/places.js
+++ controllers/places.js
@@ -30,13 +30,6 @@ router.get('/', (req, res) => {
     res.render('places/index', {places})
 });
 
-
-
-// // Get /edit.
-// router.get('/', (req, res) => {
-//     res.render('places/edit.jsx')
-// });
-
 // Get /show.
 router.get('/:id', (req, res) => {
   let id=Number(req.params.id)
@@ -47,8 +40,28 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else{
-    res.render('places/show.jsx', {place: places[id]})
+    res.render('places/show.jsx', {place: places[id], id})
   }
+});
+
+// Get /edit.
+router.get('/edit', (req, res) => {
+    res.send('EDIT /edit stub')
+});
+
+// Get /delete.
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)){
+      res.render('error404')
+    }
+    else if (!places[id]){
+      res.render('error404')
+    }
+    else{
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
 });
 
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,31 @@
 const React = require('react');
 const Def = require('../default.jsx');
 
-function show(){
+function show(data){
     return(
         <Def>
             <main>
-                <h1>PLACES SHOW PAGE</h1>
+                <h1>{data.place.name}</h1>
+                <div>
+                    <h2>Ratings</h2>
+                    <p>Currently Unrated</p>
+                </div>
+                <div>
+                    <h2>Comments</h2>
+                    <p>No comments yet!</p>
+                </div>
+                <div>
+                    <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                        Edit
+                    </a>
+                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                        <button type="submit" className="btn btn-danger">
+                            DELETE
+                        </button>
+                    </form>
+                </div>
             </main>
+            <h4>Currently Unrated</h4>
         </Def>
     )
 };
