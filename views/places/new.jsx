7e55caa6529e66505fabdc35fccbf459e03dd0a7
tjs6595 <t.schultejans@schenckprocess.com>
2022-06-21 21:16:20 -0500

    Added validations and defaults to the schema.

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -23,9 +23,9 @@ function new_form(){
                         <label htmlFor="state">State</label>
                         <input className="form-control" id="state" name="state" />
                     </div>
-                    <div className="form-group">
+                    <div className="form-group col-sm-4">
                         <label htmlFor="founded">Founded Year</label>
-                        <input className="form-control" id="founded" name="founded" />
+                        <input type="number" className="form-control" id="founded" name="founded" value={new Date().getFullYear()}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="cuisines">Cuisines</label>
